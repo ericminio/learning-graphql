9ac01899d6013ca4f6d2fdc45adf27fe9fdf2830
ericminio <eric.mignot@gmail.com>
2022-01-03 11:18:37 -0800

    identation

--- fetching-data.spec.js
+++ fetching-data.spec.js
@@ -4,46 +4,46 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { request } = require('./http-request')
   
-  describe('fetching data', () => {
+describe('fetching data', () => {
 
-    let server;
-    beforeEach((done) => {
-      let schema = buildSchema(`
-            type Query {
-                greetings: String
-            }
-        `);
-      let resolver = {
-          greetings: () => {
-              return 'hello world';
-          },
-      };
-      let app = express();
-      app.use('/api', graphqlHTTP({
-        schema: schema,
-        rootValue: resolver,
-      }));
-      server = app.listen(4000, () => {
-        done();
-      });
-    })
-    afterEach(() => {
-      server.close()
-    })
-
-    it('can be done with a POST request', (done) => {
-        request({
-          hostname: 'localhost',
-          port: 4000,
-          path: `/api`,
-          method: 'POST',
-          payload: '{ greetings }' 
-        })
-          .then(response => {
-            let answer = JSON.parse(response.body)
-            expect(answer).to.deep.equal({ data: { greetings:'hello world' }})
-            done();
-          })
-          .catch(error => done(error))          
+  let server;
+  beforeEach((done) => {
+    let schema = buildSchema(`
+      type Query {
+          greetings: String
+      }
+    `);
+    let resolver = {
+      greetings: () => {
+          return 'hello world';
+      },
+    };
+    let app = express();
+    app.use('/api', graphqlHTTP({
+      schema: schema,
+      rootValue: resolver,
+    }));
+    server = app.listen(4000, () => {
+      done();
     });
+  })
+  afterEach(() => {
+    server.close()
+  })
+
+  it('can be done with a POST request', (done) => {
+      request({
+        hostname: 'localhost',
+        port: 4000,
+        path: `/api`,
+        method: 'POST',
+        payload: '{ greetings }' 
+      })
+        .then(response => {
+          let answer = JSON.parse(response.body)
+          expect(answer).to.deep.equal({ data: { greetings:'hello world' }})
+          done();
+        })
+        .catch(error => done(error))          
+  });
 });
