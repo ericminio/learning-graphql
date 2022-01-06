f0e3622f39f3cc8ccba71aa0af5fbdfc4cf4dc14
ericminio <eric.mignot@gmail.com>
2022-01-06 09:16:27 -0800

    more about apolloServer

--- 7.apollo-server.spec.js
+++ 7.apollo-server.spec.js
@@ -2,33 +2,35 @@ const { expect } = require('chai');
 const { ApolloClient, HttpLink, InMemoryCache } = require('@apollo/client/core');
 const fetch = require('cross-fetch');
 const { ApolloServer, gql } = require('apollo-server');
+var { request } = require('./http-request');
   
 describe('apollo server', () => {
 
   let apolloServer;
   beforeEach(async () => {
-    const typeDefs = gql`
-      type Book { 
-        title: String 
-      }
+    apolloServer = new ApolloServer({ 
+      typeDefs: gql`
+        type Greeting { 
+          text: String 
+        }
 
-      type Query { 
-        books: [Book] 
-      }
-    `;
-    const resolvers = {
-      Query: {
-        books: () => [ { title:'hello world' } ],
-      },
-    };
-    apolloServer = new ApolloServer({ typeDefs, resolvers });
+        type Query { 
+          greetings: Greeting 
+        }
+      `, 
+      resolvers: {
+        Query: {
+          greetings: () => ({ text:'hello world' }),
+        },
+      } 
+    });
     await apolloServer.listen({ port:4000 });
   })
   afterEach(async () => {
     await apolloServer.stop()
   });
 
-  it('can answer to single query', (done) => {
+  it('can answer to single query from ApolloClient', (done) => {
     const client = new ApolloClient({
       link: new HttpLink({
         uri: 'http://localhost:4000/',        
@@ -37,12 +39,38 @@ describe('apollo server', () => {
       cache: new InMemoryCache(),
     });
     client.query({
-      query: gql`query GetBooks { books { title } }`
+      query: gql`{ greetings { text } }`
     })
       .then(response => {
-        expect(response.data).to.deep.equal({ books: [{ title:'hello world', __typename: "Book" }] })
+        expect(response.data).to.deep.equal({ 
+          greetings: { 
+            text:'hello world', 
+            __typename: "Greeting" 
+          } 
+        })
         done();
       })
       .catch(error => done(error))          
   });
+
+  it('can answer to single query from native HTTP POST', (done) => {
+    request({
+      hostname: 'localhost',
+      port: 4000,
+      path: `/`,
+      method: 'POST',
+      payload: JSON.stringify({ query: '{ greetings { text } }'}),
+      contentType: 'application/json',
+    })
+      .then(response => {
+        let answer = JSON.parse(response.body)
+        expect(answer.data).to.deep.equal({ 
+          greetings: { 
+            text: 'hello world'
+          }
+        })
+        done();
+      })
+      .catch(error => done(error))          
+});
 });
