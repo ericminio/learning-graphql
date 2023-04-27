27a57d1bf2d6a87b8a3c4068fd086e42ddaee162
ericminio <eric.mignot@gmail.com>
2023-04-26 20:22:42 -0700

    about errors

--- 4.calling-mutation.spec.js
+++ 4.calling-mutation.spec.js
@@ -2,52 +2,56 @@ const { expect } = require('chai');
 const { buildSchema } = require('graphql');
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-var { request } = require('./http-request')
-  
+var { request } = require('./http-request');
+
 describe('calling mutation', () => {
+    let server;
+    beforeEach((done) => {
+        let schema = buildSchema(`
+          type Query {
+            ignoredButNeeded: String
+          }
 
-  let server;
-  beforeEach((done) => {
-    let schema = buildSchema(`
-      type Query {
-        ignoedButNeeded: String
-      }
-
-      type Mutation {
-        welcome(name: String): String
-      }
-    `);
-    let resolver = {
-      welcome: (visitor) => {
-        return `hello ${visitor.name}`;
-      },
-    };
-    let app = express();
-    app.use('/api', graphqlHTTP({
-      schema: schema,
-      rootValue: resolver,
-    }));
-    server = app.listen(4000, () => {
-      done();
+          type Mutation {
+            welcome(name: String): String
+          }
+        `);
+        let resolver = {
+            welcome: (visitor) => {
+                return `hello ${visitor.name}`;
+            },
+        };
+        let app = express();
+        app.use(
+            '/api',
+            graphqlHTTP({
+                schema: schema,
+                rootValue: resolver,
+            })
+        );
+        server = app.listen(4000, () => {
+            done();
+        });
     });
-  })
-  afterEach(() => {
-    server.close()
-  })
+    afterEach(() => {
+        server.close();
+    });
 
-  it('requires the mutation keyword in the client query', (done) => {
-      request({
-        hostname: 'localhost',
-        port: 4000,
-        path: `/api`,
-        method: 'POST',
-        payload: 'mutation { welcome(name:"Joe") }'
-      })
-        .then(response => {
-          let answer = JSON.parse(response.body)
-          expect(answer).to.deep.equal({ data: { welcome:'hello Joe' }})
-          done();
+    it('requires the mutation keyword in the client query', (done) => {
+        request({
+            hostname: 'localhost',
+            port: 4000,
+            path: `/api`,
+            method: 'POST',
+            payload: 'mutation { welcome(name:"Joe") }',
         })
-        .catch(error => done(error))          
-  });
+            .then((response) => {
+                let answer = JSON.parse(response.body);
+                expect(answer).to.deep.equal({
+                    data: { welcome: 'hello Joe' },
+                });
+                done();
+            })
+            .catch((error) => done(error));
+    });
 });
--- 5.error-detection.spec.js
+++ 5.error-detection.spec.js
@@ -2,52 +2,67 @@ const { expect } = require('chai');
 const { buildSchema } = require('graphql');
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-var { request } = require('./http-request')
-  
+var { request } = require('./http-request');
+
 describe('error detection', () => {
+    let server;
+    beforeEach((done) => {
+        let schema = buildSchema(`
+          type Query {
+            ignoedButNeeded: String
+          }
 
-  let server;
-  beforeEach((done) => {
-    let schema = buildSchema(`
-      type Query {
-        ignoedButNeeded: String
-      }
-
-      type Mutation {
-        welcome(name: String): String
-      }
-    `);
-    let resolver = {
-      welcome: (visitor) => {
-        return `hello ${visitor.name}`;
-      },
-    };
-    let app = express();
-    app.use('/api', graphqlHTTP({
-      schema: schema,
-      rootValue: resolver,
-    }));
-    server = app.listen(4000, () => {
-      done();
+          type Mutation {
+            welcome(name: String): String
+          }
+        `);
+        let resolver = {
+            welcome: (visitor) => {
+                return `hello ${visitor.name}`;
+            },
+        };
+        let app = express();
+        app.use(
+            '/api',
+            graphqlHTTP({
+                schema: schema,
+                rootValue: resolver,
+            })
+        );
+        server = app.listen(4000, () => {
+            done();
+        });
     });
-  })
-  afterEach(() => {
-    server.close()
-  })
+    afterEach(() => {
+        server.close();
+    });
 
-  it('reports field mismatch on mutation call', (done) => {
-      request({
-        hostname: 'localhost',
-        port: 4000,
-        path: `/api`,
-        method: 'POST',
-        payload: 'mutation { welcome(random:"Joe") }'
-      })
-        .then(response => {
-          let answer = JSON.parse(response.body)
-          expect(answer.errors[0].message).to.equal('Unknown argument "random" on field "Mutation.welcome".')
-          done();
+    it('reports field mismatch on mutation call', (done) => {
+        request({
+            hostname: 'localhost',
+            port: 4000,
+            path: `/api`,
+            method: 'POST',
+            payload: 'mutation { welcome(random:"Joe") }',
         })
-        .catch(error => done(error))          
-  });
+            .then((response) => {
+                let answer = JSON.parse(response.body);
+                expect(answer).to.deep.equal({
+                    errors: [
+                        {
+                            locations: [
+                                {
+                                    column: 20,
+                                    line: 1,
+                                },
+                            ],
+                            message:
+                                'Unknown argument "random" on field "Mutation.welcome".',
+                        },
+                    ],
+                });
+                done();
+            })
+            .catch((error) => done(error));
+    });
 });
