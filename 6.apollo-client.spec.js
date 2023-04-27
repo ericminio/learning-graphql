e8eca45b80c815ec57575a5af5e667353f605370
ericminio <eric.mignot@gmail.com>
2023-04-26 20:50:47 -0700

    apollo client throws on missing query

--- 6.apollo-client.spec.js
+++ 6.apollo-client.spec.js
@@ -2,89 +2,148 @@ const { expect } = require('chai');
 const { buildSchema } = require('graphql');
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-const { ApolloClient, gql, HttpLink, InMemoryCache } = require('@apollo/client/core');
+const {
+    ApolloClient,
+    gql,
+    HttpLink,
+    InMemoryCache,
+} = require('@apollo/client/core');
 const { BatchHttpLink } = require('@apollo/client/link/batch-http');
 const fetch = require('cross-fetch');
-  
+
 describe('apollo client', () => {
+    let server;
+    let contentType;
+    let batchSize;
+    beforeEach(async () => {
+        let app = express();
+        app.use(express.json());
+        app.use((req, res, next) => {
+            contentType = req.headers['content-type'];
+            next();
+        });
+        app.use('/batch', (req, res, next) => {
+            batchSize = req.body.length;
+            req.body = req.body[0];
+            next();
+        });
+        app.use(
+            '/',
+            graphqlHTTP({
+                schema: buildSchema(`type Query { greetings: String } `),
+                rootValue: { greetings: 'hello world' },
+            })
+        );
+        server = await app.listen(4000);
+    });
+    afterEach(() => {
+        server.close();
+    });
 
-  let server;
-  let contentType;
-  let batchSize;
-  beforeEach(async () => {
-    let app = express();
-    app.use(express.json());
-    app.use((req, res, next) => {
-      contentType = req.headers['content-type']
-      next();
+    it('needs a fetch function', (done) => {
+        const client = new ApolloClient({
+            link: new HttpLink({
+                uri: 'http://localhost:4000/',
+                fetch,
+            }),
+            cache: new InMemoryCache(),
+        });
+        client
+            .query({
+                query: gql`
+                    {
+                        greetings
+                    }
+                `,
+            })
+            .then((response) => {
+                expect(response.data).to.deep.equal({
+                    greetings: 'hello world',
+                });
+                done();
+            })
+            .catch((error) => done(error));
     });
-    app.use('/batch', (req, res, next) => {
-      batchSize = req.body.length;
-      req.body = req.body[0];
-      next();
-    });
-    app.use('/', graphqlHTTP({
-      schema: buildSchema(`type Query { greetings: String } `),
-      rootValue: { greetings: 'hello world' },
-    }));  
-    server = await app.listen(4000);
-  })
-  afterEach(() => {
-    server.close()
-  });
 
-  it('needs a fetch function', (done) => {
-    const client = new ApolloClient({
-      link: new HttpLink({
-        uri: 'http://localhost:4000/',        
-        fetch,
-      }),
-      cache: new InMemoryCache(),
+    it('uses application/json content type', () => {
+        const client = new ApolloClient({
+            link: new HttpLink({
+                uri: 'http://localhost:4000/',
+                fetch,
+            }),
+            cache: new InMemoryCache(),
+        });
+        client
+            .query({
+                query: gql`
+                    {
+                        greetings
+                    }
+                `,
+            })
+            .then(() => {
+                expect(contentType).to.equal('application/json');
+                done();
+            })
+            .catch((error) => done(error));
     });
-    client.query({
-      query: gql`{ greetings }`
-    })
-      .then(response => {
-        expect(response.data).to.deep.equal({ greetings:'hello world' })
-        done();
-      })
-      .catch(error => done(error))          
-  });
 
-  it('uses application/json content type', () => {
-    const client = new ApolloClient({
-      link: new HttpLink({
-        uri: 'http://localhost:4000/',        
-        fetch,
-      }),
-      cache: new InMemoryCache(),
+    it('can send a batch of queries', (done) => {
+        const client = new ApolloClient({
+            link: new BatchHttpLink({
+                uri: 'http://localhost:4000/batch',
+                fetch,
+            }),
+            cache: new InMemoryCache(),
+        });
+        client
+            .query({
+                query: gql`
+                    {
+                        greetings
+                    }
+                `,
+            })
+            .then((response) => {
+                expect(batchSize).to.equal(1);
+                expect(response.data).to.deep.equal({
+                    greetings: 'hello world',
+                });
+                done();
+            })
+            .catch((error) => done(error));
     });
-    client.query({
-      query: gql`{ greetings }`
-    })
-      .then(() => {
-        expect(contentType).to.equal('application/json');
-        done();
-      })
-      .catch(error => done(error))      
-  });
 
-  it('can send a batch of queries', (done) => {
-    const client = new ApolloClient({
-      link: new BatchHttpLink({
-        uri: 'http://localhost:4000/batch',        
-        fetch,
-      }),
-      cache: new InMemoryCache(),
+    it('throws on missing query', async () => {
+        const client = new ApolloClient({
+            link: new HttpLink({
+                uri: 'http://localhost:4000/',
+                fetch,
+            }),
+            cache: new InMemoryCache(),
+        });
+        try {
+            await client.query({
+                query: gql`
+                    {
+                        unknown
+                    }
+                `,
+            });
+            throw 'should fail';
+        } catch (error) {
+            expect(error).to.deep.include({
+                message: 'Response not successful: Received status code 400',
+                graphQLErrors: [],
+                clientErrors: [],
+            });
+            expect(error.networkError).to.deep.include({
+                name: 'ServerError',
+                statusCode: 400,
+            });
+            expect(error.networkError.result.errors[0]).to.deep.include({
+                message: 'Cannot query field "unknown" on type "Query".',
+            });
+        }
     });
-    client.query({
-      query: gql`{ greetings }`
-    })
-      .then(response => {
-        expect(batchSize).to.equal(1);
-        expect(response.data).to.deep.equal({ greetings:'hello world' })
-        done();
-      })
-      .catch(error => done(error))          
-  });
 });
